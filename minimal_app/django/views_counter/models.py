d8a90612e11409acff765d7ca611325cc1ac0ca6
dmitrymailk <dmitrymail_k@mail.ru>
2022-09-03 00:25:55 +0300

    add date field

--- minimal_app/django/views_counter/models.py
+++ minimal_app/django/views_counter/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
 
 class Counter(models.Model):
     my_count = models.IntegerField(default=0)
+    current_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return str(self.my_count)
