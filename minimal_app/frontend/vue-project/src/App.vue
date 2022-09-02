58d01370f144f93fae8a4c418a88917e5c7b1083
dmitrymailk <dmitrymail_k@mail.ru>
2022-09-03 00:25:32 +0300

    update vue project

--- minimal_app/frontend/vue-project/src/App.vue
+++ minimal_app/frontend/vue-project/src/App.vue
@@ -1,13 +1,9 @@
-
 <template>
   <div>
-    <h1>
-      Simple Views Counter
-    </h1>
+    <h1>Simple Views Counter</h1>
+    <h1>Current date {{ current_date }}</h1>
     <h1>Counter amount: {{ views }}</h1>
   </div>
-
-  
 </template>
 
 <script>
@@ -15,16 +11,18 @@ import axios from "axios";
 export default {
   data() {
     return {
-      views: 0
-    }
+      views: 0,
+      current_date: "- - -",
+    };
   },
   async mounted() {
-    const domain = import.meta.env.VITE_DOMAIN 
+    const domain = import.meta.env.VITE_DOMAIN;
     console.log(domain);
-  
-    const counter = await axios.get(`${domain}/api/views-counter/`)
-    console.log(counter.data.my_count);
+
+    const counter = await axios.get(`${domain}/api/views-counter/`);
+    console.log(counter.data);
     this.views = counter.data.my_count;
-  }
-}
+    this.current_date = counter.data.current_date;
+  },
+};
 </script>
